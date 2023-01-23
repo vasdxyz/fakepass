1effe1b10df659b8751f3080ac247dde8957f7ae
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2023-01-23 12:44:30 +0200

    Update ExampleFakePass.cs

--- Runtime/FakePass/Passes/ExampleFakePass.cs
+++ Runtime/FakePass/Passes/ExampleFakePass.cs
@@ -4,30 +4,30 @@ using UnityEngine.Rendering.HighDefinition;
 
 namespace Xyz.Vasd.FakePass.Passes
 {
-    [ExecuteInEditMode]
-    public class ExampleFakePass : FakePassBase
+[ExecuteInEditMode]
+public class ExampleFakePass : FakePassBase
+{
+    protected override object Source => this;
+
+    // Inject in Setup
+    [FakePass(CustomPassInjectionPoint.AfterOpaqueDepthAndNormal, FakePassStage.Setup)]
+    private void Setup_AfterDepth(ScriptableRenderContext renderContext, CommandBuffer cmd)
     {
-        protected override object Source => this;
-
-        // Inject in Setup
-        [FakePass(CustomPassInjectionPoint.AfterOpaqueDepthAndNormal, FakePassStage.Setup)]
-        private void Setup_AfterDepth(ScriptableRenderContext renderContext, CommandBuffer cmd)
-        {
-            Debug.Log("Setup");
-        }
-
-        // Inject in Execute
-        [FakePass(CustomPassInjectionPoint.AfterOpaqueDepthAndNormal, FakePassStage.Execute)]
-        private void Execute_AfterDepth(CustomPassContext ctx)
-        {
-            //Debug.Log("Execute");
-        }
-
-        // Inject in Cleanup
-        [FakePass(CustomPassInjectionPoint.AfterOpaqueDepthAndNormal, FakePassStage.Cleanup)]
-        private void Cleanup_AfterDepth()
-        {
-            Debug.Log("Cleanup");
-        }
+        Debug.Log("Setup");
     }
+
+    // Inject in Execute
+    [FakePass(CustomPassInjectionPoint.AfterOpaqueDepthAndNormal, FakePassStage.Execute)]
+    private void Execute_AfterDepth(CustomPassContext ctx)
+    {
+        //Debug.Log("Execute");
+    }
+
+    // Inject in Cleanup
+    [FakePass(CustomPassInjectionPoint.AfterOpaqueDepthAndNormal, FakePassStage.Cleanup)]
+    private void Cleanup_AfterDepth()
+    {
+        Debug.Log("Cleanup");
+    }
+}
 }
